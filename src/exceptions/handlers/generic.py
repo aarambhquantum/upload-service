59f36adb8e94a2c7a28921f1c42da6c1c275020b
adarsh-goswamii <my@personal>
2024-11-14 22:47:10 +0530

    video, topics, section, course table created

--- src/exceptions/handlers/generic.py
+++ src/exceptions/handlers/generic.py
@@ -6,7 +6,6 @@ from starlette.responses import JSONResponse
 def register_handlers(app):
   @app.exception_handler(FileUploadException)
   async def file_upload_exception_handler(request: Request, exception: FileUploadException):
-    print("Exception handler triggered!")
     return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, content={
       "status": "failure",
       "code": status.HTTP_422_UNPROCESSABLE_ENTITY,
--- src/services/upload/controller.py
+++ src/services/upload/controller.py
@@ -2,14 +2,17 @@ from fastapi import Request, UploadFile
 from src.lib.s3 import AWS_S3
 from src.exceptions.errors.generic import FileUploadException
 from src.configs.error_constants import ErrorMessages
+from db.session import select_all
+from src.schema.main import Video
 
 class UploadController:
   """Upload Controller"""
   
   @classmethod
   async def upload(self, request: Request, payload: UploadFile):
-    print("TYPE", payload.content_type)     
     if not payload.content_type.startswith('video/'):
       raise FileUploadException(ErrorMessages.FILE_FORMAT_ERROR)
     s3 = AWS_S3()
     return await s3.upload_to_s3(payload.file, 'videos/sample')
+    
+    
